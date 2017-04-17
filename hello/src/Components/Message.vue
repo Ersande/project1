297736633d21a9a8b4d5529d216951bda3f7f578
Tiziano Mandelli <tmmande@gmail.com>
2017-04-17 15:56:35 +0100

    vuejs webpack and vue-cli

---
+++ hello/src/Components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+		<div class="box">
+			<p>
+				<slot></slot>	
+			</p>
+		</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style>
+	.box{
+		padding: 10px;
+		background: #e3e3e3;
+		border:1px solid #c5c5c5;
+	}
+
+</style>
