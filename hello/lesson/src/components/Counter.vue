67a10a478a4d896dc959a9c71f8b4204c5223ac6
Tiziano Mandelli <tmmande@gmail.com>
2017-04-17 16:13:15 +0100

    vuejs webpack cli real time data

---
+++ hello/lesson/src/components/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h2>
+			The cou is {{counter }}
+		</h2>
+		<p>
+			<input type="button" @click="counter+=50" value="Increment value" name="">
+		</p>
+	</div>
+</template>
+<script>
+	export default{
+		data(){
+			return {counter : 0};
+		}
+	}
+</script>
